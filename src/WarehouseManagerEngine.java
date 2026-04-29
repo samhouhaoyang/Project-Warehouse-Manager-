c10eac80b1a2660c709f3b405cb4584226b2e007
Sam Hou <samhouhaoyang@gmail.com>
2026-04-29 21:40:37 +1000

    scanner + main menu initial functionality

--- src/WarehouseManagerEngine.java
+++ src/WarehouseManagerEngine.java
@@ -21,9 +21,42 @@ public class WarehouseManagerEngine {
      * @param args command line args to the program
      */
     public static void main(String[] args) {
-        WarehouseManagerEngine engine = new WarehouseManagerEngine();
+        //WarehouseManagerEngine engine = new WarehouseManagerEngine();
 
         //Add your code here
+
+        if(args.length != 3){
+            System.out.println("Invalid number of Command Line Arguments. Usage: java WarehouseManagerEngine <rows> <cols> <seed>");
+        }
+        if (Integer.parseInt(args[0]) < 4 || Integer.parseInt(args[1]) < 4){
+            System.out.println("Error: Rows and columns must be at least 4 to allow proper map layout. ");
+        }
+        else{
+            int rows = Integer.parseInt(args[0]);
+            int cols = Integer.parseInt(args[1]);
+            int seed = Integer.parseInt(args[2]);
+
+            boolean running = true;
+
+            Messages.printWelcome();
+            while(running){
+                Messages.printMainMenuCommands();
+                String input = SCANNER.nextLine();
+                int option = Integer.parseInt(input);
+
+                switch (option){
+                    case 1:System.out.println("option 1"); break;
+                    case 2:System.out.println("option 2");  break;
+                    case 3:System.out.println("option 3");  break;
+                    case 4:System.out.println("option 4");  break;
+                    case 5:System.out.println("option 5");   break;
+                    default: System.out.println("Error: Invalid option. Please try again.");
+                }
+            }
+
+
+        }
+
 
     }
 
